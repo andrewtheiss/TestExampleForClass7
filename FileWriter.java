88289e6befc15d826dadb28c3c3cb0f7bacc0904
Andrew Theiss <andrew.theiss@gmail.com>
2023-08-25 14:17:54 -0700

    Summary of most recent changes (added a close function)

--- FileWriter.java
+++ FileWriter.java
@@ -2,4 +2,8 @@ public class FileWriter {
     public void write(String message) {
         System.out.println("Writing to file: " + message);
     }
+
+    public void close() {
+        System.out.println("Closing file writer");
+    }
 }
